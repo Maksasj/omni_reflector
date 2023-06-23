204cf925960f3660e40de09c64e82ed299da3d10
maksasj <radioboos@gmail.com>
2023-06-23 18:30:31 +0300

    type name thing ?

--- src/reflected.h
+++ src/reflected.h
@@ -1,13 +1,27 @@
 #ifndef _OMNI_REFLECTOR_REFLECTED_H_
 #define _OMNI_REFLECTOR_REFLECTED_H_
 
-namespace omni::reflector {
+template<typename T>
+static char* get_type_name() {
+	char *ename, *name = (char*) typeid(T).name(); 
+	ename = name;
 
+	while(*ename != '\0' && *ename != ':')
+		++ename;
+
+    return (*ename == ':') ? ename + 2 : name;
+}
+
+namespace omni::reflector {
     template<class T>
     struct Reflected {
         using Type = T;
+
+        static const char* clearTypeName;
     };
     
+    template<class T>
+    const char* Reflected<T>::clearTypeName = get_type_name<T>();
 }
 
 #endif
